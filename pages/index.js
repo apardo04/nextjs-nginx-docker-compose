b95986c5b239e20415438643b9e65344bdbd8c2c
apardo04 <apardo04@gmail.com>
2020-02-05 13:04:42 -0500

    printing out dynamic server host name instead of static

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
-const Index = () => (
-    <React.Fragment>
-    <div className="bg-img"><p className="text">My IP address: 3.86.110.201</p></div>    
-      <style jsx>{`
-          .bg-img {
-            background: url(/img/docker.jpg) no-repeat center fixed;
-            background-size: cover;
-            height: 855px;
-          }
-          .text {
-            font-size: 40px;
-            margin: 0;
-          }
-      `}</style>
-    </React.Fragment>
-  );
-  
-  export default Index;
+
+
+const Index = ({host}) => (
+  <React.Fragment>
+  <div className="bg-img"><p className="text">My IP address: {host}</p></div>    
+    <style jsx>{`
+        .bg-img {
+          background: url(/img/docker.jpg) no-repeat center fixed;
+          background-size: cover;
+          height: 855px;
+        }
+        .text {
+          font-size: 40px;
+          margin: 0;
+        }
+    `}</style>
+  </React.Fragment>
+);
+
+Index.getInitialProps = async ctx => {
+  return { host: ctx.req.headers.host }
+}
+
+export default Index;
