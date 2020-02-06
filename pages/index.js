5a1565a0204771caad5ed79a3cee74f8458aedcc
apardo04 <apardo04@gmail.com>
2020-02-06 11:46:02 -0500

    adding IP, although this only shows docker IP

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 
 
-const Index = ({host}) => (
+const Index = ({host, ip}) => (
   <React.Fragment>
-  <div className="bg-img"><p className="text">My IP address: {host}</p></div>    
+  <div className="bg-img">
+    <p className="text">Server IP address: {ip}</p>
+    <p className="text">Host: {host}</p>
+    <p className="text">WebServer01</p>
+  </div>    
     <style jsx>{`
         .bg-img {
           background: url(/img/docker.jpg) no-repeat center fixed;
@@ -12,13 +16,24 @@ const Index = ({host}) => (
         .text {
           font-size: 40px;
           margin: 0;
+          width: 36%
         }
     `}</style>
   </React.Fragment>
 );
 
+
 Index.getInitialProps = async ctx => {
-  return { host: ctx.req.headers.host }
+  try {
+    var ip = require('os').networkInterfaces().eth0[0].address;
+    console.log(ip)
+    return { ip: ip, host: ctx.req.headers.host }
+  }
+  catch(error) {
+    console.error(error)
+    return { ip: "error", host: ctx.req.headers.host }
+  }
+  
 }
 
 export default Index;
